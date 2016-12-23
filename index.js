1a066683feb7921b0f0ba66336335ca2de2bf381
arivin <btc022003@gmail.com>
2016-12-23 11:38:27 +0800

    实现最简单的上传效果,页面优化以及细节完善待续

--- index.js
+++ index.js
@@ -1,15 +1,46 @@
 var express = require('express')
 var morgan = require('morgan')
 var app = express()
-
+var bodyParser = require('body-parser')
+var fs = require('fs')
 //终端logger
 app.use(morgan('dev'))
+
+//引用body-parser插件
+app.use(bodyParser.urlencoded(
+    {
+        extended:false,
+        limit:'10mb' ///上传内容大小限制 默认100k
+    })
+)
+app.use(bodyParser.json({limit:'10mb'}))
 
 app.use(express.static('./public'))
 
 //app.get('/',(req,res)=>{
 //    res.send('app init success!!!!')
 //})
+// 上传文件数据接口
+app.post('/common/api/file_upload',(req,res)=>{
+    var imgData = req.body.imgData
+    //为了防止文件同名,使用时间戳加随机数的方式命名文件
+    var fileName = Date.now().toString()+Math.ceil(Math.random()*10000)+".jpg"
+    //传递的数据进行替换
+    var base64Data = imgData.replace(/^data:image\/\w+;base64,/,"")
+    var dataBuffer = new Buffer(base64Data,'base64')
+    fs.writeFile('./public/upload/'+fileName,dataBuffer,err=>{
+        if(err){
+            console.log(err)
+        }
+        else{
+            res.json({
+                status:'y',
+                msg:'保存成功',
+                data:fileName
+            })
+        }
+    })
+})
 
 app.listen(3000,()=>{
     console.log('服务器运行于3000端口...')
--- public/js/h5-upload.js
+++ public/js/h5-upload.js
@@ -1,7 +1,27 @@
 $.fn.extend(
     {'h5Upload':function(msg){
+        $('#fileUpload').change(function(){
+            console.dir(this)
+            var reader = new FileReader()
+            reader.readAsDataURL(this.files[0])
+            reader.onload = function(){
+                console.log(this)
+                $('#img').attr("src",this.result)
+                $.ajax({
+                    method:'post',
+                    data:{imgData:this.result},
+                    url:'/common/api/file_upload',
+                    success:function(res){
+                        console.dir(res)
+                    },
+                    error:function(err){
+                        console.dir(err)
+                    }
+                })
+            }
+        })
         this.click(function(){
-            doAlert(msg)
+            $('#fileUpload').click()
         })
         function doAlert(msg){
             alert(msg)
