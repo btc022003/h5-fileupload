49771b5357c2cf2f2cf77e0489dc3425a841e172
arivin <btc022003@gmail.com>
2016-12-23 11:58:58 +0800

    注释相关代码,调整代码布局

--- public/js/h5-upload.js
+++ public/js/h5-upload.js
@@ -1,31 +1,38 @@
 $.fn.extend(
-    {'h5Upload':function(msg){
-        $('#fileUpload').change(function(){
-            console.dir(this)
-            var reader = new FileReader()
-            reader.readAsDataURL(this.files[0])
-            reader.onload = function(){
-                console.log(this)
-                $('#img').attr("src",this.result)
-                $.ajax({
-                    method:'post',
-                    data:{imgData:this.result},
-                    url:'/common/api/file_upload',
-                    success:function(res){
-                        console.dir(res)
-                    },
-                    error:function(err){
-                        console.dir(err)
-                    }
-                })
+    {
+        'h5Upload':function(msg){
+            $('#fileUpload').change(function(){
+                console.dir(this)
+                // 通过FileReader读取文件
+                var reader = new FileReader()
+                reader.readAsDataURL(this.files[0])
+                reader.onload = function(){
+                    console.log(this)
+                    $('#img').attr("src",this.result)
+                    $.ajax({
+                        method:'post',
+                        data:{imgData:this.result},
+                        url:'/common/api/file_upload',
+                        success:function(res){
+                            console.dir(res)
+                        },
+                        error:function(err){
+                            console.dir(err)
+                        }
+                    })
+                }
+            })
+            this.click(function(){
+                $('#fileUpload').click()
+            })
+            //function doAlert(msg){
+            //    alert(msg)
+            //}
+            //图片压缩待实现
+            function compresssImg(){
+
             }
-        })
-        this.click(function(){
-            $('#fileUpload').click()
-        })
-        function doAlert(msg){
-            alert(msg)
         }
     }
-    })
+)
 $('#upload').h5Upload("我是upload")
